f4acc551661cb4931a5ac556379582d1559387cd
FlorientM <florient.michel@ext.adeo.com>
2023-12-05 14:49:56 +0100

    Update functions and wishlist test creation

--- tests/pages/ProductPage.ts
+++ tests/pages/ProductPage.ts
@@ -1,14 +1,21 @@
 import { expect, Locator, Page } from '@playwright/test';
+import exp from 'constants';
 
 export class ProductPage {
     readonly page: Page;
     readonly addToCart: Locator;
     readonly seeMyCart: Locator;
+    readonly addToWishlist: Locator;
+    readonly inputCreateWishlist: Locator;
+    readonly validateWishlistButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.addToCart = page.getByTestId('BTN_addtocart');
         this.seeMyCart = page.getByTestId('BTN_Voir-mon-panier');
+        this.addToWishlist = page.getByTestId('BTN_addToShoppingList');
+        this.inputCreateWishlist = page.getByTestId('INPUT_createShoppingList');
+        this.validateWishlistButton = page.getByTestId('BTN_submitAddToShoppingList');
     }
 
     async addProductToCart() {
@@ -20,5 +27,22 @@ export class ProductPage {
     async seeTheCart() {
         await expect(this.seeMyCart).toBeEnabled;
         await this.seeMyCart.click();
+    }
+
+    async addProductToWishlist() {
+        await expect(this.addToWishlist).toBeEnabled;
+        await this.addToWishlist.focus();
+        await this.addToWishlist.click();
+    }
+
+    async createWishlistName() {
+        await expect(this.inputCreateWishlist).toBeEditable;
+        await this.inputCreateWishlist.click();
+        await this.inputCreateWishlist.pressSequentially('WishlistFlo');
+    }
+
+    async validateWishlistCreation() {
+        await expect(this.validateWishlistButton).toBeEnabled;
+        await this.validateWishlistButton.click();
     }
 }
---
+++ tests/wishlist.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+import { MenuPage } from './pages/menuPage';
+import { LoginPage } from './pages/LoginPage';
+import { ProductPage } from './pages/ProductPage';
+import { OrderTunnel } from './pages/OrderTunnel';
+import { getDataLib } from '../utils/datasetManager';
+import moment from 'moment';
+
+test.use({ userAgent: 'LMUser Cerberus' });
+
+test('go to the homepage', async({ page }) => {
+        const menuPage = new MenuPage(page);
+        const loginPage = new LoginPage(page);
+        const productPage = new ProductPage(page);
+        const orderTunnel = new OrderTunnel(page);
+        const loyalty = page.locator('//*[@class="ipn-loyalty-card-no-loyalty"]');
+        // const checkoutReview = page.getByRole('heading', { name: 'Récapitulatif' })
+        const checkoutReview = page.locator('//*[@class="confirmation-header-message"]');
+        const customer = await getDataLib('LM-CUSTOMER');
+        const product = await getDataLib('LM-1P-DATASET');
+
+        test.step('Go to Homepage', async () => {await menuPage.goto()});
+        await menuPage.validateConsentCookies();
+        await menuPage.goToAccountLogin();
+        await loginPage.login(customer.user.email, customer.user.password);
+        // Les deux lignes suivantes servent à attendre que la page ait fini de charger sinon le robot va trop vite
+        await loyalty.waitFor();
+        await expect(loyalty).toBeVisible();
+        await menuPage.goToAProductPage(product.product.RNC1P);
+        await productPage.addProductToWishlist();
+        await productPage.createWishlistName();
+        await productPage.validateWishlistCreation();
+
+        let dateString = moment().format("YYYYMMDD-HHmmss");
+
+        await page.screenshot({ path: 'test-results/screenshots/'+ dateString + '.png', fullPage: true });
+});
