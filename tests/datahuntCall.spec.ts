cca9cbda0f606dfe1e18f8769eb23b75c7a7b07b
FlorientM <florient.michel@ext.adeo.com>
2023-12-21 18:43:11 +0100

    Update API call

--- tests/datahuntCall.spec.ts
+++ tests/datahuntCall.spec.ts
@@ -2,9 +2,12 @@ import { test, expect } from '@playwright/test';
 import { DatahuntApi, OrderDeliveryMethod } from '../pages/DatahuntApi';
 import { getEnvVariable } from '../utils/shared';
 
+// À mettre en en-tête pour exécuter en série
+test.describe.configure({mode: 'serial'});
+
 test.describe.serial("Make an order", () => {
         let context = {};
-        let order_id: any;
+        let jsonDatahuntRefList: any;
 
         //Before Collection (pre-request script)
         /*
@@ -26,8 +29,9 @@ test.describe.serial("Make an order", () => {
             
         });*/
         
-
-    test('Get offer from Datahunt', async({ request }) => {
+    // À faire : Merger Datahunt et ATP
+    test('Get offer from Datahunt and validate in ATP', async({ request }) => {
+        /*
         let datahunt = new DatahuntApi(request);
         
         // API Call
@@ -37,28 +41,29 @@ test.describe.serial("Make an order", () => {
         await expect(response.status()).toBe(200);
 
         // Data stocking for next tests
-        order_id = await response.json();
-        console.log(order_id);
+        jsonDatahuntRefList = await response.json();
+        console.log(jsonDatahuntRefList);
         
-        console.log(order_id[0]);
-    });
+        console.log(jsonDatahuntRefList[0]);
+        */
+    // });
     
-    test('ATP.ATP - Prevalidate offer', async({ request }) => {
+    // test('ATP.ATP - Prevalidate offer', async({ request }) => {
         // Pre request script
-        let order_id2 = order_id[0];
+        // let jsonDataFirstRef = jsonDatahuntRefList[0];
         let options = {
             headers: {
                 "x-gateway-apikey": getEnvVariable('APIKEY_ATP'),
                 "x-bu-code": getEnvVariable('BU')
             },
             data: {
-                order_id: order_id,
-                body: {
+                
                     "channel": "WEB",
                     "shoppingCartLines": [
                       {
                         "id": "1",
-                        "productReference": order_id2[0],
+                        // "productReference": jsonDataFirstRef[0],
+                        "productReference": '79948260',
                         "quantity": 1
                       }
                     ],
@@ -70,20 +75,49 @@ test.describe.serial("Make an order", () => {
                       "id": "176",
                       "buId": "001"
                     }
-                }
+                
             }
         };
 
         // API call
-        let response = await request.post("https://scdp-uat1.priv.nprd.api.devportal.adeo.cloud/api-atp-available-to-promise/v1/available-to-promises", options);
-        console.log(await response.text());
-        await expect(response.status()).toBe(200);
-        console.log(response.status());
+        let responseATP = await request.post("https://scdp-uat1.priv.nprd.api.devportal.adeo.cloud/api-atp-available-to-promise/v1/available-to-promises", options);
+        expect(responseATP.status()).toBe(200);
+        let atpJson = await responseATP.json();
+        // console.log('Réponse entière : ' + JSON.stringify(atpJson));
+        
+        let pickUpFound = false;
+        let deliveryList = atpJson.deliverySimulations[0]?.availableToPromises;
+        for (let index = 0; index < deliveryList.length; index++) {
+          const element = deliveryList[index];
+          // console.log(JSON.stringify(element.deliveryService.mode));        
+          if (element.deliveryService.mode == 'PICKUP_IN_STORE') {
+            pickUpFound = true;
+          }
+        }
+        /*
+        if (pickUpFound) {
+          console.log('Gagné')
+        } else {
+          console.log('Perdu')
+        }
+        */
+        
+        let foundDelivery = deliveryList.find(x => {
+          return x.deliveryService.mode == 'PICKUP_IN_STORE';
+      });
+        console.log(foundDelivery);
 
-        let responseBody = await response.json();
+        if (foundDelivery) {
+          console.log('Gagné')
+        } else {
+          console.log('Perdu')
+        }
+
+        /*
+        let responseBody = await responseATP.json();
         console.log(responseBody);
-        order_id = responseBody.order_id;
-
+        let order_id = responseBody.order_id;
+        */
         // Asserts
 
         // Data stocking for next tests
